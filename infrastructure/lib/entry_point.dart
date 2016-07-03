9daa983878a1ac056937f6a36576ad02e83863b1
Luke Church <lukechurch@google.com>
2016-07-03 18:25:19 +0100

    Remove delay from sample

--- infrastructure/lib/entry_point.dart
+++ infrastructure/lib/entry_point.dart
@@ -10,6 +10,5 @@ import 'dart:async';
 
 Future<String> sintrEntryPoint(String msg) async {
   // throw "Please replace the entry point";
-  await new Duration(milliseconds: 5000);
   return "Sample string-$msg";
 }

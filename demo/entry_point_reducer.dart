60820dbae362bbcd74f15a2fb1c8f7389e4c12a2
Luke Church <lukechurch@google.com>
2016-07-11 19:27:05 +0100

    Add demo code and data

---
+++ demo/entry_point_reducer.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This is the code that will be replaced by the infrastructure when
+// the job is actually executed
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+Future<String> sintrEntryPoint(String msg) async {
+  Map kvs = JSON.decode(msg);
+  List vs = kvs.values.first;
+  return JSON.encode(vs.length);
+}

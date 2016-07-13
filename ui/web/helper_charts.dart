a736f395dc98fb0d340bbd56d18cacbb7f00f0fd
Mariana Marasoiu <mariana.marasoiu@gmail.com>
2016-07-13 21:38:35 +0100

    Add copyright notice to new files.

--- ui/web/helper_charts.dart
+++ ui/web/helper_charts.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 part of sintr_ui;
 
 /// The data to be visualised. It's updated everytime new data comes from the server.

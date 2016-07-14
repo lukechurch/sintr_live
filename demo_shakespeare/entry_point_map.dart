523269f4e8b71dbbf384e315ce4115c0e31c592a
Luke Church <lukechurch@google.com>
2016-07-14 18:22:10 +0100

    Shakespeare demo

---
+++ demo_shakespeare/entry_point_map.dart
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This is the code that will be replaced by the infrastructure when
+// the job is actually executed
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+Future<String> sintrEntryPoint(String msg) async {
+  String text = msg;
+
+  List<String> words = text.split(new RegExp(r"(\s+)"));
+  List<Map<String, int>> kvs = [];
+
+  int i = 0;
+
+  for (String word in words) {
+    if (++i % 100000 == 0) break;
+    kvs.add({word: 1});
+  }
+
+  return JSON.encode(kvs);
+}
---
+++ demo_shakespeare/entry_point_reducer.dart
@@ -0,0 +1,25 @@
+// Copyright (c) 2016, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// This is the code that will be replaced by the infrastructure when
+// the job is actually executed
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+Future<String> sintrEntryPoint(String msg) async {
+  List<Map> kvList = JSON.decode(msg);
+  Map<String, int> kvMap = {};
+  kvList.forEach((Map<String, int> kv) {
+    String word = kv.keys.first;
+    int count = kv.values.first;
+    kvMap.putIfAbsent(word, () => 0);
+    kvMap[word] = kvMap[word] + count;
+  });
+
+  List<Map<String, int>> result = [];
+  kvMap.forEach((String word, int count) => result.add({'word': word, 'count': count}));
+  return JSON.encode(result);
+}
